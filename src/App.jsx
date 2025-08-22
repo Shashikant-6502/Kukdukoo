e0e549e7bc45a8b23e4674b6111d9a7d7510f880
innovativenavneet <navneet.prakash1233@gmail.com>
2025-08-22 23:38:06 +0530

    updated Arch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
 import './style.css'; 
 
 function App() {
   return (
     <Router>
-      <Header />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
       </main>
-      <Footer />
     </Router>
   );
 }
