8d8bf8cbb9e2c7761be6d8c3209df80a589b4036
Shashikant-6502 <Shashikant6502@gmail.com>
2025-08-28 23:19:50 +0530

    removing mumbai

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import ContactPage from './pages/ContactPage';
 import TicketsPage from './pages/TicketsPage';
 import Header from './components/common/Header/Header';
 import Footer from './components/common/Footer/Footer';
-import MumbaiCityPage from "./pages/MumbaiCityPage";
 import './style.css'; 
 function App() {
   return (
@@ -22,7 +21,6 @@ function App() {
               <Route path="/sponsor" element={<SponsorPage />} />
               <Route path="/contact" element={<ContactPage />} />
               <Route path="/tickets" element={<TicketsPage />} />
-              <Route path="/Mumbai" element={<MumbaiCityPage/>} />
             </Routes>
           </div>
         </main>
